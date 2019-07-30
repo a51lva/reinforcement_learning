29444a256d89f28b38c59355b0007ef307cddfa4
Acilio Silva <eng.acilio@gmail.com>
2019-07-30 16:15:21 +0100

    add reward history avg plot function

--- BanditMachine/main.py
+++ BanditMachine/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from bandit import Bandit as Bandit
 from agent import Agent as Agent
 
@@ -15,14 +16,23 @@ def experiment(bandit, agent, n_episodes):
         reward_history.append(reward)
     return (np.array(action_history), np.array(reward_history))
 
+def plotRewardHistoryResult(reward_history_avg, experiments, episodes, epsilon):
+    plt.plot(reward_history_avg)
+    plt.xlabel('Episode Number')
+    plt.ylabel('Rewards Colleted'.format(experiments))
+    plt.title('Bandit reward history averaged over {} experiments (epsilon = {})'.format(experiments, epsilon))
+    ax = plt.gca()
+    ax.set_xscale("log", nonposx='clip')
+    plt.xlim([1, episodes])
+    plt.show()
 
 if __name__ == "__main__":
     print('Hello My Awesome Casino Machine')
 
     bandit_probs = [0.10, 0.50, 0.60, 0.80, 0.10, 0.25, 0.60, 0.45, 0.75, 0.65]
     experiments = 100
-    episodes = 10000
-    epsilon = 0.1
+    episodes = 100
+    epsilon = 0.8
 
     n_bandits = len(bandit_probs)
     print("Running multi-armed bandits with N_bandits = {} and agent epsilon = {}".format(n_bandits, epsilon))
@@ -47,7 +57,7 @@ if __name__ == "__main__":
         
         for j, (a) in enumerate(action_history):
             action_history_sum[j][a] += 1
-    
+
     reward_history_avg /= np.float(experiments)
     print("reward history avg = {}".format(reward_history_avg))        
-        
+    plotRewardHistoryResult(reward_history_avg, experiments, episodes, epsilon)   
